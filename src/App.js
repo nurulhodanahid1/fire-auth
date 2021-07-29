7ca51ae1050b8d172dae20fb813cfd337e4f8b8e
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-07-29 14:51:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig)
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: "",
+    email: "",
+    photo: ""
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result)
+        const { displayName, email, photoURL } = result.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser)
+        console.log(displayName, email, photoURL)
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message)
+      })
+  }
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then(result => {
+      const signOutUser = {
+        isSignedIn: false,
+        name: "",
+        email: "",
+        photo: ""
+      }
+      setUser(signOutUser)
+      console.log(result)
+    })
+    .catch(err => {
 
-export default App;
+    })
+  }
+  return (
+      <div className="App">
+        {
+          user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button>
+                          : <button onClick={handleSignIn}>Sign in</button>
+        }
+        {
+          user.isSignedIn && <div>
+            <p>Welcome, <strong>{user.name}</strong></p>
+            <p>Your email address is: {user.email}</p>
+            <img src={user.photo} alt="" />
+          </div>
+        }
+      </div>
+    );
+  }
+
+  export default App;
